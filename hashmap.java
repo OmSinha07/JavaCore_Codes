d630cf1efa7425c64ae505b0d2898cd797c5af2a
Om Sinha <omaasinha.99@gmail.com>
2024-06-14 11:46:35 +0530

    New Change

--- hashmap.java
+++ hashmap.java
@@ -2,12 +2,15 @@ import java.util.*;
 import java.util.Map.*;
 public class hashmap {
    public static void main(String[] args) {
-    HashMap<Integer,String>tm=new HashMap<>(Map.of(0,"A",1,"B",2,"C"));
-    tm.put(3,"D");
-    tm.put(5,"D");
+    HashMap<String,Integer>tm=new HashMap<>(Map.of("A",1,"B",2));
+    HashMap<String,Integer>tm2Map=new HashMap<>(Map.of("A",1,"B",2));
 
+
+    tm.put("D",5);
+    tm.put("D",6);
+    tm.put("E",tm2Map.getOrDefault("A", 0)+1);
   
-    System.out.println(tm.get(2));
+    System.out.println(tm.get("E"));
 
    } 
 }
